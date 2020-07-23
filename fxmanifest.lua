f75a16359b5c187e6bef9bf82c156ea040f1c8dc
Nikez <nikez141@gmail.com>
2020-07-23 15:03:36 +0200

    feat: initial draft of documentation for sound occlusion in custom interiors

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file 'AUDIO_GAMEDATA' 'audio/v_shop_247.dat' -- dat151
+data_file 'AUDIO_DYNAMIXDATA' 'audio/v_shop_247_mix.dat' -- dat15
+
+files {
+  'audio/v_shop_247_game.dat151.rel',
+  'audio/v_shop_247_mix.dat15.rel',
+}
